19bd4aa6d66eb7a1657626f04d1b81e290ba8cb8
Harsha Krishnappa <Harsha.x.Krishnappa.-ND@disney.com>
2018-09-04 16:01:38 +0530

    Initial Commit

---
+++ JenkinsPOC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JenkinsPOC
+//
+//  Created by Harsha K on 04/09/18.
+//  Copyright © 2018 Harsha.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
